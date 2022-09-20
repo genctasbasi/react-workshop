4234481369f5f2fed797d61157a024b99fde3e95
Genc Tasbasi <gmtgmt@gmail.com>
2022-09-20 11:43:09 +0100

    - React Redux example - Updates to food order app

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ This repo hosts some simple applications to explain React concepts. Some of the 
 
 Enjoy.
 
-## Is Basket! - UseReducer hook
-
-A simple application to demonstrate UseReducer hook. Here is the artice [on Medium.](https://tasbasi.medium.com/react-usereducer-hook-made-simple-1df1f9838bb9)
-
-## Counter - UseState hook
-
-React counter to demonstrate UseState hook.
-
 ## Food order app
 
 This is a practice application from [this udemy course.](https://www.udemy.com/course/react-the-complete-guide-incl-redux)
+
+## react-reducer: A mini game to demonstrate UseReducer hook
+
+A simple application to demonstrate UseReducer hook. Here is the artice [on Medium.](https://tasbasi.medium.com/react-usereducer-hook-made-simple-1df1f9838bb9)
+
+## react-counter: UseState hook
+
+React counter to demonstrate UseState hook.
--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -10,12 +10,26 @@ const defaultCartState = {
 // this is the 'reducer' function
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    // we aren't editing the existing state here but
-    // creating a new array of updated items.
-    const updatedItems = state.items.concat(action.item);
-
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
 
     return {
       items: updatedItems,
